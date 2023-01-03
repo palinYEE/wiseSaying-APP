b693b4f8d3790410143f4877767b4cf268ffdd9f
Yoon Yeoung-jin <alwns28@kookmin.ac.kr>
2023-01-03 21:02:09 +0900

    [code] 시작 화면 및 navigation bar 추가

---
+++ wiseSaying/wiseSaying/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  wiseSaying
+//
+//  Created by yoon-yeoungjin on 2023/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private func backgroundViewImageSetting() {
+        // main view 에서 백그라운드 이미지 셋팅하는 함수 추가 필요
+    }
+    
+    private func navigationBarSetting() {
+        self.title = "wiseSaying"
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.backgroundViewImageSetting()
+        navigationBarSetting()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
